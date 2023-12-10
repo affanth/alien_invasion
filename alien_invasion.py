161359da6457303152fbfe94f253a38893f866f2
Affanth <affan0409@icloud.com>
2023-12-10 12:55:52 -0500

    .gitignore

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
+
 class AlienInvasion:
 
     
@@ -20,11 +22,14 @@ class AlienInvasion:
         self.ship = Ship(self)
         
         self.bg_color = (234,12,70)
+
+        self.bullets = pygame.sprite.Group()
     
     def run_game(self):
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
 
@@ -44,6 +49,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
         
             
 
@@ -53,12 +60,16 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
     def _update_screen(self):
 
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
-
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         pygame.display.flip()
 
--- settings.py
+++ settings.py
@@ -4,5 +4,9 @@ class Settings:
         self.ship_speed = 1.5
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (0,100,100)
+        self.bg_color = (180,20,60)
         
+        self.bullet_speed = 1000.0
+        self.bullet_width = 3
+        self.bullet_height= 15
+        self.bullet_color = (8, 37, 21)
